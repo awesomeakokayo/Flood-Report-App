015b86442107304a6866e00388ce9a0e293af430
awesomeakokayo <awesomeakokayo@gmail.com>
2026-01-25 14:46:11 -0800

    modified:   src/services/api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 const BASE_URL = Platform.select({
-    android: 'http://10.0.2.2:8000',
-    ios: 'http://localhost:8000',
-    default: 'http://localhost:8000',
+  android: "https://flood-report-api.onrender.com",
+  ios: "https://flood-report-api.onrender.com",
+  default: "https://flood-report-api.onrender.com",
 });
 
 const api = axios.create({
